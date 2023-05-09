b92510795511be49a30b8c72f1c88b02c33cc010
FerValenzuela-ops <fernandox_240997@live.com>
2023-05-09 11:53:34 -0400

    first commit

---
+++ src/test/java/com/example/cameltrigger/CamelTriggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cameltrigger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelTriggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
